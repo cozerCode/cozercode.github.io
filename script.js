5437c1f7840e91ebef51865cda65b13f7ac4e2b2
cozerCode <cozer.code1199@gmail.com>
2021-12-30 22:19:51 +0100

    Major Commit

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-hi 
+var typing_index = 0;
+var txt = "I'm a programmer, a game developer and a website designer!";
+var speed = 100;
+
+function type() {
+    var text_element = document.getElementById("typing");
+
+    if(typing_index < 58) {
+        text_element.innerHTML += txt.charAt(typing_index);
+        typing_index++;
+    }
+
+    setTimeout(type,speed);
+}
+
+type();
